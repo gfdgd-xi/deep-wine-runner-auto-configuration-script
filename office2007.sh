042b404936f8a42d4e5d66718015c25f116a0426
gfdgd_xi <3025613752@qq.com>
2022-10-05 09:24:09 +0800

    添加2007

---
+++ office2007.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+echo Power By $MAKER
+echo $COPYRIGHT
+rm -rfv /tmp/office2007.7z
+download https://code.gitlink.org.cn/gfdgd_xi/microsoft-office-library/raw/branch/master/microsoft-office-2007-professional-plus.7z?gfdgd-xi /tmp office2007.7z
+mkdir -p /tmp/office2007
+7z x /tmp/office2007.7z -o/tmp/office2007
+bat /tmp/office2007/setup.exe
+info 提示 安装完成！
