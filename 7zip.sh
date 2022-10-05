5e1fe6d18b9f372b74826a099064c49f15f493a6
gfdgd_xi <3025613752@qq.com>
2022-10-05 22:04:41 +0800

    winrar

---
+++ 7zip.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+rm /tmp/7z2201.exe
+download https://code.gitlink.org.cn/gfdgd_xi/wine-runner-list/raw/branch/master/other/7z2201.exe /tmp 7z2201.exe
+bat /tmp/7z2201.exe
+info 提示 安装完成！
---
+++ winrar.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+rm /tmp/winrar-x32-611scp.exe
+download https://code.gitlink.org.cn/gfdgd_xi/wine-runner-list/raw/branch/master/other/winrar-x32-611scp.exe /tmp winrar-x32-611scp.exe
+bat /tmp/winrar-x32-611scp.exe
+info 提示 安装完成！
