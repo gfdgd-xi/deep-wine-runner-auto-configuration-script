d72647472b686153c1d3c36548a9b6806f72e01c
gfdgd_xi <3025613752@qq.com>
2022-10-12 21:37:31 +0800

    新增360safe极速版

---
+++ 360safe-fast.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 操作
+rm -rf /tmp/360safejisu+333765+yq.exe
+download http://urlqh.cn/mViAF /tmp 360safejisu+333765+yq.exe
+bat /tmp/360safejisu+333765+yq.exe
+info 提示 安装完成！
--- 360zip.sh
+++ 360zip.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env deepin-wine-runner-auto-install-bash
-# 用于判断是否为 bash 解释器
-# 旧版解释器直接提示不兼容
-if [[ 1 == 2 ]]; then
-    error 错误 此脚本只支持\ Wine\ 运行器\ 2.3.0\ 及以上版本，请更新至最新版本
-    exit
-fi
 # 操作
+rm -rf /tmp/360zip_yqlm_333765.exe
 download http://urlqh.cn/mVjcJ /tmp 360zip_yqlm_333765.exe
 bat /tmp/360zip_yqlm_333765.exe
 info 提示 安装完成！
