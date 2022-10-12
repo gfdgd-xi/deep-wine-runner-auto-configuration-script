5778cd14d6bd95cba71fe6ed45e19a91212685cd
gfdgd_xi <3025613752@qq.com>
2022-10-12 22:03:03 +0800

    2345

---
+++ 2345browser.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 操作
+rm -rf /tmp/2345explorer_k3025613752.exe
+download https://jifendownload.2345.cn/jifen_2345/2345explorer_k3025613752.exe /tmp 2345explorer_k3025613752.exe
+bat /tmp/2345explorer_k3025613752.exe
+info 提示 安装完成！
