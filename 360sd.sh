0842a619058d1277439a8a6f77c7339f60135ec4
gfdgd_xi <3025613752@qq.com>
2022-10-12 21:49:18 +0800

    360sd

---
+++ 360sd.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 操作
+rm -rf /tmp/360sd_333765.exe
+download http://urlqh.cn/mTZSZ /tmp 360sd_333765.exe
+bat /tmp/360sd_333765.exe
+info 提示 安装完成！
