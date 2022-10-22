cb51cbf79d2fcc54196952e2c5ff77100e66b64d
gfdgd_xi <3025613752@qq.com>
2022-10-22 17:27:32 +0800

    可以简易添加作者源

---
+++ qnchrome.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 操作
+rm -rf /tmp/qnchrome_k3025613752.exe
+download https://jifendownload.2345.cn/jifen_2345/qnchrome_k3025613752.exe /tmp qnchrome_k3025613752.exe
+bat /tmp/qnchrome_k3025613752.exe
+info 提示 安装完成！
