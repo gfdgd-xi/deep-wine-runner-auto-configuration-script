cad324751ba3fd1dbff22f8c17c91a998f6130cb
gfdgd_xi <3025613752@qq.com>
2022-11-03 13:15:40 +0800

    文件加密大师

---
+++ 文件加密大师.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2022年10月28日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    bash rm /tmp/文件夹加密超级大师.exe
+fi
+rm /tmp/文件夹加密超级大师.exe
+download https://www.gitlink.org.cn/api/gfdgd_xi/program-library/raw?filepath=%E6%96%87%E4%BB%B6%E5%A4%B9%E5%8A%A0%E5%AF%86%E8%B6%85%E7%BA%A7%E5%A4%A7%E5%B8%88.exe&ref=master /tmp 文件夹加密超级大师.exe
+bat /tmp/文件夹加密超级大师.exe
+info 提示 安装完成！
