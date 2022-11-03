a0c5fad6105a4ec1e860e26101dc7ae241be1547
gfdgd_xi <3025613752@qq.com>
2022-11-03 13:30:30 +0800

    new

--- purgeexepro.sh
+++ purgeexepro.sh
@@ -1,5 +1,7 @@
 if [[ 1 == 2 ]]; then
-    pkexec apt purge com.super.wine -y
-    pkexec apt purge com.exepro -y
+    bash pkexec apt purge com.super.wine -y
+    bash pkexec apt purge com.exepro -y
 fi
+pkexec apt purge com.super.wine -y
+pkexec apt purge com.exepro -y
 info 提示 已删除超级\ Wine\ 和\ exepro！
