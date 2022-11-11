e332f7c415d32c33da6892775b5f398404f70dec
gfdgd_xi <3025613752@qq.com>
2022-11-11 12:24:27 +0800

    Bandizip

---
+++ Bandizip.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 操作
+rm -rf /tmp/BANDIZIP-SETUP-STD-X64_7.27.0.1.EXE
+download https://webcdn.m.qq.com/spcmgr/download/BANDIZIP-SETUP-STD-X64_7.27.0.1.EXE /tmp BANDIZIP-SETUP-STD-X64_7.27.0.1.EXE
+bat /tmp/BANDIZIP-SETUP-STD-X64_7.27.0.1.EXE
+info 提示 安装完成！
