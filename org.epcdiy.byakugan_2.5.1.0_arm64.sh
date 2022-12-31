90778049efab36210d8ba6e8e7ad6b4c9a6d0d7a
gfdgd_xi <3025613752@qq.com>
2022-12-31 22:11:36 +0800

    新增程序 安装 已经打包完成的 白眼（基于 Deepin Wine6 Stable，适用于 arm64 机器）

---
+++ org.epcdiy.byakugan_2.5.1.0_arm64.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2022年12月31日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    download "https://code.gitlink.org.cn/gfdgd_xi/gfdgd-xi-apt-mirrors/raw/branch/master/wine/org.epcdiy.byakugan/org.epcdiy.byakugan_2.5.1.0_arm64.deb" /tmp "org.epcdiy.byakugan_2.5.1.0_arm64.deb"
+    pkexec apt install "/tmp/org.epcdiy.byakugan_2.5.1.0_arm64.deb" -y
+    info 提示 安装成功！
+    exit
+fi
+download "https://code.gitlink.org.cn/gfdgd_xi/gfdgd-xi-apt-mirrors/raw/branch/master/wine/org.epcdiy.byakugan/org.epcdiy.byakugan_2.5.1.0_arm64.deb" /tmp "org.epcdiy.byakugan_2.5.1.0_arm64.deb"
+if [[ -f "/tmp/org.epcdiy.byakugan_2.5.1.0_arm64.deb" ]]; then
+    # Nothing
+    echo Download Done!
+else
+    error 错误 文件下载失败！
+    exit
+fi
+pkexec apt install "/tmp/org.epcdiy.byakugan_2.5.1.0_arm64.deb" -y
+if [[ $? == 0 ]]; then
+    info 提示 安装成功！
+else
+    error 错误 安装失败！
+fi
+rm -rfv "/tmp/org.epcdiy.byakugan_2.5.1.0_arm64.deb"
