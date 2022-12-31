7b974ca6bff4f6d4fc02422509f6e989b0c7c558
gfdgd_xi <3025613752@qq.com>
2022-12-31 22:30:23 +0800

    新增程序 安装 射手播放器2020

---
+++ SPlayer-Setup-4.9.4.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2022年12月31日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/SPlayer-Setup-4.9.4.exe"
+fi
+rm "/tmp/SPlayer-Setup-4.9.4.exe"
+download "https://code.gitlink.org.cn/gfdgd_xi/program-library/raw/branch/master/SPlayer-Setup-4.9.4.exe?gfdgd_xi=gfdgd_xi" /tmp "SPlayer-Setup-4.9.4.exe"
+bat "/tmp/SPlayer-Setup-4.9.4.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/SPlayer-Setup-4.9.4.exe"
