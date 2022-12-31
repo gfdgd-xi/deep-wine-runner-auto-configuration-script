7dd0fdeba5f4b3c74b2ff4e95030ee4070f926fb
gfdgd_xi <3025613752@qq.com>
2022-12-31 21:39:16 +0800

    新增程序 体验 NTPWEdit 0.7 32位

---
+++ ntpwedit.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2022年12月31日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/ntpwedit.exe"
+fi
+rm "/tmp/ntpwedit.exe"
+download "https://code.gitlink.org.cn/gfdgd_xi/program-library/raw/branch/master/ntpwedit.exe?gfdgd_xi=gfdgd_xi" /tmp "ntpwedit.exe"
+bat "/tmp/ntpwedit.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/ntpwedit.exe"
