a1686e6b88c46119b7641eb59d46cd68ba4ce834
gfdgd_xi <3025613752@qq.com>
2023-01-01 20:56:55 +0800

    新增程序 安装 TeamViewer Amd64

---
+++ TeamViewer_Setup_x64.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月1日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/TeamViewer_Setup_x64.exe"
+fi
+rm "/tmp/TeamViewer_Setup_x64.exe"
+download "https://download.teamviewer.com/download/TeamViewer_Setup_x64.exe" /tmp "TeamViewer_Setup_x64.exe"
+bat "/tmp/TeamViewer_Setup_x64.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/TeamViewer_Setup_x64.exe"
