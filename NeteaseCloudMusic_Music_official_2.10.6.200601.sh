c9cd2de19bd116224f238ee542a0a0a53b4f6193
gfdgd_xi <3025613752@qq.com>
2023-01-01 13:54:09 +0800

    新增程序 安装 网易云音乐2.10.6.200601

---
+++ NeteaseCloudMusic_Music_official_2.10.6.200601.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月1日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/NeteaseCloudMusic_Music_official_2.10.6.200601.exe"
+fi
+rm "/tmp/NeteaseCloudMusic_Music_official_2.10.6.200601.exe"
+download "https://d1.music.126.net/dmusic/NeteaseCloudMusic_Music_official_2.10.6.200601.exe" /tmp "NeteaseCloudMusic_Music_official_2.10.6.200601.exe"
+bat "/tmp/NeteaseCloudMusic_Music_official_2.10.6.200601.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/NeteaseCloudMusic_Music_official_2.10.6.200601.exe"
