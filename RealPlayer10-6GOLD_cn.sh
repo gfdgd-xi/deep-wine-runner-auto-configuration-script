09f11de7892f81862cd251878461c6ef2bbf905d
gfdgd_xi <3025613752@qq.com>
2023-01-01 13:46:31 +0800

    新增程序 安装 RealPlayer 10

---
+++ RealPlayer10-6GOLD_cn.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月1日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/RealPlayer10-6GOLD_cn.exe"
+fi
+rm "/tmp/RealPlayer10-6GOLD_cn.exe"
+download "https://code.gitlink.org.cn/gfdgd_xi/program-library/raw/branch/master/RealPlayer10-6GOLD_cn.exe?gfdgd_xi=gfdgd_xi" /tmp "RealPlayer10-6GOLD_cn.exe"
+bat "/tmp/RealPlayer10-6GOLD_cn.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/RealPlayer10-6GOLD_cn.exe"
