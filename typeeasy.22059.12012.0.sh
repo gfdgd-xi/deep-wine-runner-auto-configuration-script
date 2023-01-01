4b16a593c1936899a8a919130c1075f490bae90a
gfdgd_xi <3025613752@qq.com>
2023-01-01 14:04:12 +0800

    新增程序 安装 金山打字通2016

---
+++ typeeasy.22059.12012.0.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月1日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/typeeasy.22059.12012.0.exe"
+fi
+rm "/tmp/typeeasy.22059.12012.0.exe"
+download "https://package.op.wpscdn.cn/wps/download/typeeasy.22059.12012.0.exe" /tmp "typeeasy.22059.12012.0.exe"
+bat "/tmp/typeeasy.22059.12012.0.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/typeeasy.22059.12012.0.exe"
