33d0b177bebd312b533209376621d9f8c50b636f
gfdgd_xi <3025613752@qq.com>
2023-01-01 13:33:01 +0800

    新增程序 安装 ZYH 浏览器（易语言版）

---
+++ ZYHBroswer_Setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月1日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/ZYHBroswer_Setup.exe"
+fi
+rm "/tmp/ZYHBroswer_Setup.exe"
+download "https://code.gitlink.org.cn/gfdgd_xi/program-library/raw/branch/master/ZYHBroswer_Setup.exe?gfdgd_xi=gfdgd_xi" /tmp "ZYHBroswer_Setup.exe"
+bat "/tmp/ZYHBroswer_Setup.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/ZYHBroswer_Setup.exe"
