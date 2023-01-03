52eece90c2d81f27ae43a0c8ab5487dded19691d
gfdgd_xi <3025613752@qq.com>
2023-01-03 19:12:14 +0800

    新增程序 安装 360极速浏览器 13.5.2036 32位版本

--- 2345OCR_v1.2.0.228.sh
+++ 2345OCR_v1.2.0.228.sh
@@ -2,11 +2,15 @@
 # 使用 Wine 运行器的语言解析器
 ##########################################################################################
 # 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
-# 更新时间：2023年1月3日
+# 更新时间：2023年1月1日
 ##########################################################################################
 # 用于判断是否为 bash 解释器
 if [[ 1 == 2 ]]; then
     # 保持对旧版本的兼容
-    bash xdg-open 'https://download.2345.cn/pdfcvt/ocr/2345OCR_v1.2.0.228.exe'
+    bash rm "/tmp/QQPCDownload320001.exe"
 fi
-xdg-open 'https://download.2345.cn/pdfcvt/ocr/2345OCR_v1.2.0.228.exe' 
+rm "/tmp/QQPCDownload320001.exe"
+download "https://download.2345.cn/pdfcvt/ocr/2345OCR_v1.2.0.228.exe" /tmp "QQPCDownload320001.exe"
+bat "/tmp/QQPCDownload320001.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/QQPCDownload320001.exe"
--- 360cse_13.5.2036.0.sh
+++ 360cse_13.5.2036.0.sh
@@ -2,15 +2,15 @@
 # 使用 Wine 运行器的语言解析器
 ##########################################################################################
 # 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
-# 更新时间：2023年1月1日
+# 更新时间：2023年1月3日
 ##########################################################################################
 # 用于判断是否为 bash 解释器
 if [[ 1 == 2 ]]; then
     # 保持对旧版本的兼容
-    bash rm "/tmp/QQPCDownload320001.exe"
+    bash rm "/tmp/360cse_13.5.2036.0.exe"
 fi
-rm "/tmp/QQPCDownload320001.exe"
-download "https://download.2345.com/2345pcsafe/background/20221223/2345pcsafe_v7.12.exe" /tmp "QQPCDownload320001.exe"
-bat "/tmp/QQPCDownload320001.exe"
+rm "/tmp/360cse_13.5.2036.0.exe"
+download "https://down.360safe.com/cse/360cse_13.5.2036.0.exe" /tmp "360cse_13.5.2036.0.exe"
+bat "/tmp/360cse_13.5.2036.0.exe"
 info 提示 安装完成！
-rm -rfv "/tmp/QQPCDownload320001.exe"
+rm -rfv "/tmp/360cse_13.5.2036.0.exe"
