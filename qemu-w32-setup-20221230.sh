da0218ea7ff4874337e1fc812338c7120aebe42d
gfdgd_xi <3025613752@qq.com>
2023-01-03 22:10:11 +0800

    新增程序 安装 Qemu 2022.12.30 32位

---
+++ qemu-w32-setup-20221230.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月3日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/qemu-w32-setup-20221230.exe"
+fi
+rm "/tmp/qemu-w32-setup-20221230.exe"
+download "https://code.gitlink.org.cn/gfdgd_xi/program-library/raw/branch/master/qemu-w32-setup-20221230.exe?gfdgd_xi=gfdgd_xi" /tmp "qemu-w32-setup-20221230.exe"
+bat "/tmp/qemu-w32-setup-20221230.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/qemu-w32-setup-20221230.exe"
