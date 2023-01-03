6c21937791b790477576fc84716303352fb8eed7
gfdgd_xi <3025613752@qq.com>
2023-01-03 09:02:18 +0800

    新增程序 安装 2345青鸟浏览器

---
+++ qnchrome_k3025613752.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月3日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash xdg-open 'https://jifendownload.2345.cn/jifen_2345/qnchrome_k3025613752.exe'
+fi
+xdg-open 'https://jifendownload.2345.cn/jifen_2345/qnchrome_k3025613752.exe' 
