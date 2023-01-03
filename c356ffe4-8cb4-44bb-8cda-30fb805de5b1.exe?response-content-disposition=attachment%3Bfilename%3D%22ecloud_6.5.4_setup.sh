fb1f6bf17658145ed388106e61ae73250f882b4d
gfdgd_xi <3025613752@qq.com>
2023-01-03 21:40:22 +0800

    新增程序 安装 天翼云盘6.5.4

---
+++ c356ffe4-8cb4-44bb-8cda-30fb805de5b1.exe?response-content-disposition=attachment%3Bfilename%3D%22ecloud_6.5.4_setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月3日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/c356ffe4-8cb4-44bb-8cda-30fb805de5b1.exe?response-content-disposition=attachment%3Bfilename%3D%22ecloud_6.5.4_setup.exe%22&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-CLOUDTYPEIN=PERSON&x-amz-limit=rate%3D51200%2Cconcurrency%3D12&x-amz-CLIENTTYPEIN=UNKNOWN&Signature=C0fV0UUfhnJrU6SM7q%2B3qslkW3E%3D&AWSAccessKeyId=f66329aba8ec74a7accc&x-amz-userLevel=0&Expires=1672756789&x-amz-FSIZE=68562456&x-amz-UID=297953828&x-amz-UFID=92333243006044723"
+fi
+rm "/tmp/c356ffe4-8cb4-44bb-8cda-30fb805de5b1.exe?response-content-disposition=attachment%3Bfilename%3D%22ecloud_6.5.4_setup.exe%22&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-CLOUDTYPEIN=PERSON&x-amz-limit=rate%3D51200%2Cconcurrency%3D12&x-amz-CLIENTTYPEIN=UNKNOWN&Signature=C0fV0UUfhnJrU6SM7q%2B3qslkW3E%3D&AWSAccessKeyId=f66329aba8ec74a7accc&x-amz-userLevel=0&Expires=1672756789&x-amz-FSIZE=68562456&x-amz-UID=297953828&x-amz-UFID=92333243006044723"
+download "https://cloud189-beijing-gpz-person.oos-hq-bj.ctyunapi.cn/c356ffe4-8cb4-44bb-8cda-30fb805de5b1.exe?response-content-disposition=attachment%3Bfilename%3D%22ecloud_6.5.4_setup.exe%22&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-CLOUDTYPEIN=PERSON&x-amz-limit=rate%3D51200%2Cconcurrency%3D12&x-amz-CLIENTTYPEIN=UNKNOWN&Signature=C0fV0UUfhnJrU6SM7q%2B3qslkW3E%3D&AWSAccessKeyId=f66329aba8ec74a7accc&x-amz-userLevel=0&Expires=1672756789&x-amz-FSIZE=68562456&x-amz-UID=297953828&x-amz-UFID=92333243006044723" /tmp "c356ffe4-8cb4-44bb-8cda-30fb805de5b1.exe?response-content-disposition=attachment%3Bfilename%3D%22ecloud_6.5.4_setup.exe%22&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-CLOUDTYPEIN=PERSON&x-amz-limit=rate%3D51200%2Cconcurrency%3D12&x-amz-CLIENTTYPEIN=UNKNOWN&Signature=C0fV0UUfhnJrU6SM7q%2B3qslkW3E%3D&AWSAccessKeyId=f66329aba8ec74a7accc&x-amz-userLevel=0&Expires=1672756789&x-amz-FSIZE=68562456&x-amz-UID=297953828&x-amz-UFID=92333243006044723"
+bat "/tmp/c356ffe4-8cb4-44bb-8cda-30fb805de5b1.exe?response-content-disposition=attachment%3Bfilename%3D%22ecloud_6.5.4_setup.exe%22&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-CLOUDTYPEIN=PERSON&x-amz-limit=rate%3D51200%2Cconcurrency%3D12&x-amz-CLIENTTYPEIN=UNKNOWN&Signature=C0fV0UUfhnJrU6SM7q%2B3qslkW3E%3D&AWSAccessKeyId=f66329aba8ec74a7accc&x-amz-userLevel=0&Expires=1672756789&x-amz-FSIZE=68562456&x-amz-UID=297953828&x-amz-UFID=92333243006044723"
+info 提示 安装完成！
+rm -rfv "/tmp/c356ffe4-8cb4-44bb-8cda-30fb805de5b1.exe?response-content-disposition=attachment%3Bfilename%3D%22ecloud_6.5.4_setup.exe%22&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-CLOUDTYPEIN=PERSON&x-amz-limit=rate%3D51200%2Cconcurrency%3D12&x-amz-CLIENTTYPEIN=UNKNOWN&Signature=C0fV0UUfhnJrU6SM7q%2B3qslkW3E%3D&AWSAccessKeyId=f66329aba8ec74a7accc&x-amz-userLevel=0&Expires=1672756789&x-amz-FSIZE=68562456&x-amz-UID=297953828&x-amz-UFID=92333243006044723"
