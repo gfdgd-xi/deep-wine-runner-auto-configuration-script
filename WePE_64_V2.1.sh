922910d88433a3b3971729fd779d827f8227bda7
gfdgd_xi <3025613752@qq.com>
2023-01-04 17:25:01 +0800

    新增程序 微PE工具箱V2.1 64位

---
+++ WePE_64_V2.1.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月4日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/WePE_64_V2.1.exe"
+fi
+rm "/tmp/WePE_64_V2.1.exe"
+download "https://mirrors.sdu.edu.cn/software/Windows/WePE/WePE_64_V2.1.exe" /tmp "WePE_64_V2.1.exe"
+bat "/tmp/WePE_64_V2.1.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/WePE_64_V2.1.exe"
