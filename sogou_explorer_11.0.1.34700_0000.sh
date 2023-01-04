e6a9d817c468e92a3bc6dca72a69279e990b0fe2
gfdgd_xi <3025613752@qq.com>
2023-01-04 17:29:11 +0800

    新增程序 搜狗浏览器11.0.1.34700

---
+++ sogou_explorer_11.0.1.34700_0000.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月4日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/sogou_explorer_11.0.1.34700_0000.exe"
+fi
+rm "/tmp/sogou_explorer_11.0.1.34700_0000.exe"
+download "https://dlie.sogoucdn.com/se/sogou_explorer_11.0.1.34700_0000.exe" /tmp "sogou_explorer_11.0.1.34700_0000.exe"
+bat "/tmp/sogou_explorer_11.0.1.34700_0000.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/sogou_explorer_11.0.1.34700_0000.exe"
