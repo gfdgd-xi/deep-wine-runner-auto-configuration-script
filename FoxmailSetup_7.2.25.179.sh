d36a2df9231b1abbf5eed09529b92db9a7761446
gfdgd_xi <3025613752@qq.com>
2023-01-04 17:40:57 +0800

    新增程序 安装 Foxmail 7.2.25

---
+++ FoxmailSetup_7.2.25.179.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月4日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/FoxmailSetup_7.2.25.179.exe"
+fi
+rm "/tmp/FoxmailSetup_7.2.25.179.exe"
+download "https://dldir1.qq.com/foxmail/windows/FoxmailSetup_7.2.25.179.exe" /tmp "FoxmailSetup_7.2.25.179.exe"
+bat "/tmp/FoxmailSetup_7.2.25.179.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/FoxmailSetup_7.2.25.179.exe"
