fcb26e9f370d44138bd801cfa47519b50957251d
gfdgd_xi <3025613752@qq.com>
2023-01-04 10:38:01 +0800

    新增程序 安装 360 TOTAL SECURITY（需要wine7及以上版本）

---
+++ 360TS_Setup_10.8.0.1520.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月4日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/360TS_Setup_10.8.0.1520.exe"
+fi
+rm "/tmp/360TS_Setup_10.8.0.1520.exe"
+download "https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.8.0.1520.exe" /tmp "360TS_Setup_10.8.0.1520.exe"
+bat "/tmp/360TS_Setup_10.8.0.1520.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/360TS_Setup_10.8.0.1520.exe"
