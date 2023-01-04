92f4018944d51d9f6f2f37b6fc078dfebd1e92d1
gfdgd_xi <3025613752@qq.com>
2023-01-04 12:41:03 +0800

    新增程序 安装 Python 3.4.4 32位（需要把Wine运行器升级到3.1.0或更高版本）

---
+++ python-3.4.4.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月4日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/python-3.4.4.msi"
+fi
+rm "/tmp/python-3.4.4.msi"
+download "https://code.gitlink.org.cn/gfdgd_xi/program-library/raw/branch/master/python-3.4.4.msi?gfdgd_xi=gfdgd_xi" /tmp "python-3.4.4.msi"
+installmsi "/tmp/python-3.4.4.msi"
+info 提示 安装完成！
+rm -rfv "/tmp/python-3.4.4.msi"
