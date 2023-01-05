cc93524fdafc6edad894eeb48a69e7c72c260716
gfdgd_xi <3025613752@qq.com>
2023-01-05 11:13:10 +0800

    新增程序 双核浏览器（需要wine7及以上版本）

---
+++ ChromeCore_1189_6.1.1.12.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月5日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/ChromeCore_1189_6.1.1.12.exe"
+fi
+rm "/tmp/ChromeCore_1189_6.1.1.12.exe"
+download "http://file.cdn.cqttech.com/file/ChromeCore_1189_6.1.1.12.exe" /tmp "ChromeCore_1189_6.1.1.12.exe"
+bat "/tmp/ChromeCore_1189_6.1.1.12.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/ChromeCore_1189_6.1.1.12.exe"
