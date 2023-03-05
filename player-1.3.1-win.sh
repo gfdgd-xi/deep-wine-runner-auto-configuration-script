a08da5276ffaa92679b4adce494c8ba5bf7f49c4
gfdgd_xi <3025613752@qq.com>
2023-03-05 17:31:14 +0800

    新增程序 安装 音乐湖 PC 端 1.3.1

---
+++ player-1.3.1-win.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年3月5日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/player-1.3.1-win.exe"
+fi
+rm "/tmp/player-1.3.1-win.exe"
+download "https://code.gitlink.org.cn/gfdgd_xi/program-library/raw/branch/master/player-1.3.1-win.exe?gfdgd_xi=gfdgd_xi" /tmp "player-1.3.1-win.exe"
+bat "/tmp/player-1.3.1-win.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/player-1.3.1-win.exe"
