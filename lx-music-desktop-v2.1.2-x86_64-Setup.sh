2022ac3a91b334301c0cd5689d44428c0be225dd
gfdgd_xi <3025613752@qq.com>
2023-03-05 17:34:40 +0800

    新增程序 安装 洛雪音乐助手桌面版 2.1.2 64位

---
+++ lx-music-desktop-v2.1.2-x86_64-Setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年3月5日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/lx-music-desktop-v2.1.2-x86_64-Setup.exe"
+fi
+rm "/tmp/lx-music-desktop-v2.1.2-x86_64-Setup.exe"
+download "https://code.gitlink.org.cn/gfdgd_xi/program-library/raw/branch/master/lx-music-desktop-v2.1.2-x86_64-Setup.exe?gfdgd_xi=gfdgd_xi" /tmp "lx-music-desktop-v2.1.2-x86_64-Setup.exe"
+bat "/tmp/lx-music-desktop-v2.1.2-x86_64-Setup.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/lx-music-desktop-v2.1.2-x86_64-Setup.exe"
