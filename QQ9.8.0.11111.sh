88aa0b0488a8cedbf734afc31f578961821e957c
gfdgd_xi <3025613752@qq.com>
2023-03-25 17:23:29 +0800

    新增程序 安装公测版 QQ 9.8.0.11111

---
+++ QQ9.8.0.11111.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年3月25日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/QQ9.8.0.11111.exe"
+fi
+rm "/tmp/QQ9.8.0.11111.exe"
+download "https://code.gitlink.org.cn/gfdgd_xi/program-library/raw/branch/master/QQ9.8.0.11111.exe?gfdgd_xi=gfdgd_xi" /tmp "QQ9.8.0.11111.exe"
+bat "/tmp/QQ9.8.0.11111.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/QQ9.8.0.11111.exe"
