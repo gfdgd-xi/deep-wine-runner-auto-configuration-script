66561db7a80cb2043575bdc679ea01be7e30396b
gfdgd_xi <3025613752@qq.com>
2023-04-29 22:34:57 +0800

    新增程序 安装 Qemu 20230424

---
+++ qemu-w64-setup-20230424.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi
+# 更新时间：2023年4月29日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/qemu-w64-setup-20230424.exe"
+fi
+rm "/tmp/qemu-w64-setup-20230424.exe"
+download "https://code.gitlink.org.cn/gfdgd_xi/program-library/raw/branch/master/qemu-w64-setup-20230424.exe?gfdgd_xi=gfdgd_xi" /tmp "qemu-w64-setup-20230424.exe"
+bat "/tmp/qemu-w64-setup-20230424.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/qemu-w64-setup-20230424.exe"
