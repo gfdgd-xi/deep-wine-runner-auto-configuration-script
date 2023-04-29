e93276a0e7f7aeb1317134287d4cb7476976ffdd
gfdgd_xi <3025613752@qq.com>
2023-04-29 10:33:59 +0800

    新增程序 安装 天翼云盘 6.5.6

---
+++ ecloud_6.5.6_setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env deepin-wine-runner-auto-install-bash
+# 使用 Wine 运行器的语言解析器
+##########################################################################################
+# 作者：gfdgd xi
+# 更新时间：2023年4月29日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/ecloud_6.5.6_setup.exe"
+fi
+rm "/tmp/ecloud_6.5.6_setup.exe"
+download "https://code.gitlink.org.cn/gfdgd_xi/program-library/raw/branch/master/ecloud_6.5.6_setup.exe?gfdgd_xi=gfdgd_xi" /tmp "ecloud_6.5.6_setup.exe"
+bat "/tmp/ecloud_6.5.6_setup.exe"
+info 提示 安装完成！
+rm -rfv "/tmp/ecloud_6.5.6_setup.exe"
